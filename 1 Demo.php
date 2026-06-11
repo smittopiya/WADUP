5419f8c7515991d53eb17af651d1c5e98d1a5db8
smittopiya <52095705+smittopiya@users.noreply.github.com>
2026-06-11 21:58:03 +0530

    Add files via upload

---
+++ 1 Demo.php
@@ -0,0 +1,15 @@
+<?php
+            $str="hello string";
+            $x=200;
+            $y=44.6;
+            echo "String is: $str <br>";
+            echo "Integer is: $x <br>";
+            echo "Float is: $y <br>";
+            echo "<br>";
+            var_dump($str); 
+            echo "<br>";
+            var_dump($x);
+            echo "<br>";
+            var_dump($y)      
+?>
+
