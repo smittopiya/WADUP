d57facfa6f3892079335f24f3f87b18392387e5d
smittopiya <52095705+smittopiya@users.noreply.github.com>
2026-06-15 21:15:37 +0530

    Add files via upload

---
+++ 3 Global.php
@@ -0,0 +1,11 @@
+<?php
+    $name = "Smit Topiya";
+    function global_var()
+    {
+        global $name;
+        echo "Variable inside the function: ".$name;
+        echo "<br>";
+    }
+    global_var();
+    echo "Variable outside the funtion: ".$name;
+?>
