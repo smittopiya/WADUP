a33373189d26695f01b0bc68c3609a74f3e9742e
smittopiya <52095705+smittopiya@users.noreply.github.com>
2026-07-03 08:58:39 +0530

    Add Result.php to calculate total and percentage

---
+++ LAB WORK 1/1 Result.php
@@ -0,0 +1,12 @@
+<?php
+    $SUB1=90;
+    $SUB2=85;
+    $SUB3=95;
+    $SUB4=100;
+    $SUB5=95;
+    $TOTAL=$SUB1+$SUB2+$SUB3+$SUB4+$SUB5;
+    $PERCENTAGE=$TOTAL/5;
+    echo "Result of Previous Semester"."<br>";
+    echo "TOTAL MARKS : ".$TOTAL."<br>";
+    echo "PERCENTAGE : ".$PERCENTAGE."%"
+?>
