ff68ca65709d4bd08e32d187f0af1e4e6522c431
smittopiya <52095705+smittopiya@users.noreply.github.com>
2026-08-04 23:35:16 +0530

    Add files via upload

---
+++ LAB WORK 1/3 Operators.php
@@ -0,0 +1,15 @@
+<?php
+
+$num1 = 20;
+$num2 = 2;
+echo "num1 : ".$num1."<br>"."num2 : ".$num2."<br>"."<br>";
+
+
+echo "Addition: " . ($num1 + $num2) . "<br>"; 
+echo "Subtraction: " . ($num1 - $num2) . "<br>"; 
+echo "Multiplication: " . ($num1 * $num2) . "<br>"; 
+echo "Division: " . ($num1 / $num2) . "<br>";
+echo "Modulus: " . ($num1 % $num2) . "<br>";
+echo "Exponentiation: " . ($num1 ** $num2) . "<br>";
+
+?>
