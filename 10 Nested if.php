67ce91408538612ed392695cd41fd79bf2ba0152
smittopiya <52095705+smittopiya@users.noreply.github.com>
2026-08-11 22:37:24 +0530

    Add files via upload

---
+++ 10 Nested if.php
@@ -0,0 +1,13 @@
+<?php 
+    $age = 23; 
+    $nationality = "Indian"; 
+    if ($nationality == "Indian") 
+    { 
+        if ($age >= 18) { 
+            echo "Eligible to give vote"; 
+        } 
+        else {    
+            echo "Not eligible to give vote"; 
+        } 
+    } 
+?> 
---
+++ 8 If else if.php
@@ -0,0 +1,23 @@
+<?php 
+    $marks=69; 
+    if ($marks<33)
+    { 
+        echo "fail"; 
+    } 
+    else if ($marks>=50 && $marks<65) 
+    { 
+        echo "C grade"; 
+    } 
+    else if ($marks>=65 && $marks<80) 
+    { 
+        echo "B grade"; 
+    } 
+    else if ($marks>=80 && $marks<90) 
+    { 
+        echo "A grade"; 
+    } 
+    else 
+    { 
+        echo "Invalid input"; 
+    } 
+?> 
