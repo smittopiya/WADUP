95636b26561e0e55e5b7b11e21ad5070aef5eb67
smittopiya <52095705+smittopiya@users.noreply.github.com>
2026-08-16 19:00:07 +0530

    Add files via upload

---
+++ PRESENTATION/1.php
@@ -0,0 +1,11 @@
+<?php
+
+// 1. array() - Creates an array
+
+echo "1. Before array():". "<br>". "\$arr does not exist\n"."<br>";
+
+$arr = array(1, 2, 3); 
+
+echo "   After array(): [", $arr[0], ", ", $arr[1], ", ", $arr[2], "]\n\n";
+
+?>
