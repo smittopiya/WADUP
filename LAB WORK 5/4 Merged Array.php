e379e58f80c3a49d142ad65e80aeb6e8a40511d6
smittopiya <52095705+smittopiya@users.noreply.github.com>
2026-08-17 23:34:30 +0530

    Add files via upload

---
+++ LAB WORK 5/4 Merged Array.php
@@ -0,0 +1,10 @@
+<?php
+
+$foods1 = array("Apple","Orange","Banana","Coconut");
+$foods2 = array("Mango","strawberry");
+
+$mergedArray = array_merge($foods1,$foods2);
+
+print_r($mergedArray);
+
+?>
